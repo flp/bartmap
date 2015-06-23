7c9b02017093ead4fb4713a46154fe41b45a385e
Franklin Pearsall <franklin.pearsall@gmail.com>
2015-06-23 13:19:48 -0700

    Realtime trains appear on the Richmond line for 4 stations near Richmond.
    Currently not possible to tell which direction a train is going from its
    icon.

--- static/bartmap/main.js
+++ static/bartmap/main.js
@@ -1,9 +1,11 @@
 var API_KEY = 'QAVH-UAY7-IQZQ-DT35'
 var BASE_URL = 'http://api.bart.gov/api'
 
-// Object that represents an estimated time of departure for a station.
-// What an estimate means: a train is estimated to leave from 'stationName' 
-// towards 'destination' in 'minutes' minutes and 'direction' direction.
+/**
+ * Object that represents an estimated time of departure for a station.
+ * What an estimate means: a train is estimated to leave from 'stationName' 
+ * towards 'destination' in 'minutes' minutes and 'direction' direction.
+ */
 var Estimate = function(stationName, stationAbbr, destination, destAbbr,
                         minutes, platform, direction, length, color, hexColor,
                         bikeFlag) {
@@ -114,8 +116,7 @@ var adjacentStationsDictionary = {
 
 // This dictionary maps adjacent stations to the average time, in minutes,
 // it takes a train to go from one station to the other.
-// Currently, only stations on the Richmond line are present and only in
-// north - south ordering.
+// Currently, only stations on the Richmond line are present.
 var stationEstimatesDictionary = {
   "richmond_el-cerrito-del-norte": 4,
   "el-cerrito-del-norte_richmond": 4,
@@ -251,45 +252,49 @@ function showStationInfo(xmlData) {
   console.log("Fetched estimates for {0}.".format(estimates[0].stationName));
 }
 
-function getAdjacentTrains(stationName, estimates) {
-  // I have all of the estimates for the station
-  // each estimate is a minute estimate for a destination and direction
-  // i want to return only the estimates that put the train between this
-  // station and an adjacent station.
-  adjacentDepartures = new AdjacentDepartures([], [], [], []);
-  adjacentStations = adjacentStationsDictionary[stationName];
-}
-
 function getAdjacentRedTrains(stationName, estimates) {
+  /**
+   * Finds estimates of adjacent trains for a station from an array of estimates.
+   * 
+   * Args:
+   *   stationName - name of station, e.g. 'richmond'.
+   *   estimates - Array of Estimates.
+   *
+   * Returns:
+   *   Dictionary mapping destinations to arrays of adjacent estimates.
+   */
   // Get adjacent stations.
-  adjacentStations = adjacentStationsDictionary[stationName];
-  adjacentMillbraeStation = adjacentStations.south;
-  adjacentRichmondStation = adjacentStations.north;
+  var adjacentStations = adjacentStationsDictionary[stationName];
+  var adjacentMillbraeStation = adjacentStations.south;
+  var adjacentRichmondStation = adjacentStations.north;
 
   // Get time distances to adjacent stations.
-  adjacentMillbraeTimeDistance = null;
-  adjacentRichmondTimeDistance = null;
+  var adjacentMillbraeTimeDistance = null;
+  var adjacentRichmondTimeDistance = null;
   if (adjacentMillbraeStation != null) {
-    key = stationName + "_" + adjacentMillbraeStation;
+    var key = stationName + "_" + adjacentMillbraeStation;
     adjacentMillbraeTimeDistance = stationEstimatesDictionary[key];
   }
   if (adjacentRichmondStation != null) {
-    key = stationName + "_" + adjacentRichmondStation;
+    var key = stationName + "_" + adjacentRichmondStation;
     adjacentRichmondTimeDistance = stationEstimatesDictionary[key];
   }
 
   // Find nearby trains.
-  adjacentEstimates = {
+  var adjacentEstimates = {
     "millbrae": [],
     "richmond": []
   }
   for (var i = 0; i < estimates.length; i++) {
-    if (estimates[i].destination.lower() == "millbrae" &&
-        parseInt(estimates[i].minutes) <= adjacentMillbraeTimeDistance) {
+    // Trains going in the millbrae direction should be compared 
+    if (estimates[i].destination.toLowerCase() == "millbrae" &&
+        parseInt(estimates[i].minutes) <= adjacentMillbraeTimeDistance &&
+        estimates[i].color == "RED") {
       adjacentEstimates["millbrae"].push(estimates[i]);
     } else if (
-        estimates[i].destination.lower() == "richmond" &&
-        parseInt(estimates[i].minutes) <= adjacentMillbraeTimeDistance) {
+        estimates[i].destination.toLowerCase() == "richmond" &&
+        parseInt(estimates[i].minutes) <= adjacentMillbraeTimeDistance &&
+        estimates[i].color == "RED") {
       adjacentEstimates["richmond"].push(estimates[i]);
     }
   }
@@ -297,10 +302,194 @@ function getAdjacentRedTrains(stationName, estimates) {
 }
 
 function getAdjacentRedTrainsTEST(stationName) {
-  bartData = getDeparturesSynchronous(stationName);
+  stationAbbr = stationDictionary[stationName];
+  bartData = getDeparturesSynchronous(stationAbbr);
   estimates = parseBartXML(bartData);
   adjacentEstimates = getAdjacentRedTrains(stationName, estimates);
   return adjacentEstimates;
+}
+
+/* ************************************************************************* */
+// DRAWING CODE
+
+/**
+ * Dictionary mapping stations to train spots data.
+ *
+ * Data is in the form of dictionaries mapping destinations to arrays of ids. Arrays are ordered by
+ * distance to station in nondescending order.
+ */
+var trainSpotsDictionary = {
+  'el-cerrito-del-norte':
+    {
+      'millbrae': ['rich-deln-0', 'rich-deln-1', 'rich-deln-2', 'rich-deln-3'],
+      'richmond': ['plza-nbrk-0', 'deln-plza-2', 'deln-plza-1']
+    },
+  'el-cerrito-plaza':
+    {
+      'millbrae': ['deln-plza-0', 'deln-plza-1', 'deln-plza-2'],
+      'richmond': ['nbrk-dbrk-0', 'plza-nbrk-3', 'plza-nbrk-2', 'plza-nbrk-1']
+    },
+  'north-berkeley':
+    {
+      'millbrae': ['plza-nbrk-0', 'plza-nbrk-1', 'plza-nbrk-2', 'plza-nbrk-3'],
+      'richmond': ['dbrk-ashb-0', 'nbrk-dbrk-1']
+    },
+  'downtown-berkeley':
+    {
+      'millbrae': ['nbrk-dbrk-0', 'nbrk-dbrk-1'],
+      'richmond': ['ashb-mcar-0', 'dbrk-ashb-1']
+    },
+}
+
+function getTrainSpots(station) {
+  /**
+   * Gets CSS ids of DOM elements where trains could be drawn for given station.
+   *
+   * Args:
+   *   station - station name, e.g. 'richmond'
+   *
+   * Returns:
+   *   Dictionary mapping destinations to arrays of ids. Arrays are ordered by distance to
+   *   station in nondescending order.
+   */ 
+   return trainSpotsDictionary[station];
+}
+
+function getLiveTrainSpots(station, estimates) {
+  /**
+   * Gets CSS ids of DOM elements where trains should be drawn given current estimates.
+   *
+   * Args:
+   *   station - station name, e.g. 'richmond'
+   *   estimates - Array of Estimates.
+   *
+   * Returns:
+   *   ids - Array of CSS ids.
+   */
+  // TODO: currently, two trains can be drawn on the same spot and will overlap one another.
+  // there should be some way to tell that two trains are at the same spot, going opposite
+  // directions.
+  console.log("Number of estimates for {0}: {1}".format(station, estimates.length));
+  var ids = [];
+  var adjacentRedTrains = getAdjacentRedTrains(station, estimates);
+  console.log("Number of adjacent estimates for {0}: {1}".format(
+    station, adjacentRedTrains['millbrae'].length + adjacentRedTrains['richmond'].length));
+  var trainSpots = getTrainSpots(station);
+  var adjacentStations = adjacentStationsDictionary[station];
+
+  // Get average time to closest northern and southern stations.
+  var northernStation = adjacentStations.north;
+  var key = station + "_" + northernStation;
+  var avgNorthTime = stationEstimatesDictionary[key];
+  var southernStation = adjacentStations.south;
+  key = station + "_" + southernStation;
+  var avgSouthTime = stationEstimatesDictionary[key];
+
+  // Use the adjacent estimates and train spots to figure out where to draw trains.
+  for (var i = 0; i < adjacentRedTrains['millbrae'].length; i++) {
+    var estMinutes = adjacentRedTrains['millbrae'][i].minutes;
+    var index = avgNorthTime - estMinutes;
+    ids.push(trainSpots['millbrae'][index]);
+  }
+  for (var i = 0; i < adjacentRedTrains['richmond'].length; i++) {
+    var estMinutes = adjacentRedTrains['richmond'][i].minutes;
+    var index = avgSouthTime - estMinutes;
+    ids.push(trainSpots['millbrae'][index]);
+  }
+
+  return ids;
+}
+
+function clearTrains() {
+  /**
+   * Removes train icons from the DOM.
+   */
+  $('.train-icon').remove();
+}
+
+function drawTrains(ids) {
+  /**
+   * Draws trains.
+   *
+   * Args:
+   *   ids - Array of CSS ids for divs to add train icons to.
+   */
+  for (var i = 0; i < ids.length; i++) {
+    var trainDiv = $('#' + ids[i]);
+    trainDiv.append('<img class="train-icon" src="/static/bartmap/train_icon.png" />');
+  }
+}
+
+// END DRAWING CODE
+/* ************************************************************************* */
+
+function run() {
+  /**
+   * Main event loop.
+   */
+  stationsToTest = ["el-cerrito-del-norte", 
+                    "el-cerrito-plaza", 
+                    "north-berkeley", 
+                    "downtown-berkeley"];
+
+  function logAdjacentTrains(stationName) {
+
+    function doWork(xmlData) {
+      estimates = parseBartXML(xmlData);
+      if (estimates.length > 1) {
+        adjacents = getAdjacentRedTrains(stationName, estimates);
+        console.log("Adjacent trains for {0}:".format(stationName));
+        millbraeAdjacents = adjacents["millbrae"];
+        richmondAdjacents = adjacents["richmond"];
+        for (var i = 0; i < millbraeAdjacents.length; i++) {
+          console.log("Destination: {0}. Time to departure from {1}: {2}".format(
+                      millbraeAdjacents[i].destination, stationName, 
+                      millbraeAdjacents[i].minutes));
+        }
+        for (var i = 0; i < richmondAdjacents.length; i++) {
+          console.log("Destination: {0}. Time to departure from {1}: {2}".format(
+                      richmondAdjacents[i].destination, stationName, 
+                      richmondAdjacents[i].minutes));
+        }
+        console.log("End output for {0}.".format(stationName));
+        console.log("-------------------------------------------------------");
+      }
+    }
+
+    return doWork;
+  }
+
+  // console.log("BEGIN NEW DATA");
+  // for (var i = 0; i < stationsToTest.length; i++) {
+  //   stationName = stationsToTest[i];
+  //   stationAbbr = stationDictionary[stationsToTest[i]];
+  // }
+
+  function drawAdjacentTrains(station) {
+
+    var fn = function(xmlData) {
+      // TODO: test this
+      // get adjacent train estimates
+      var estimates = parseBartXML(xmlData);
+      var drawIds = getLiveTrainSpots(station, estimates);
+      console.log("CSS ids to draw for {0}: {1}".format(station, drawIds));
+
+      // draw on the draw spots
+      drawTrains(drawIds);
+    }
+    return fn
+
+  }
+  
+  // TODO: gather all train data in a single request
+  clearTrains();
+  for (var i = 0; i < stationsToTest.length; i++) {
+    stationName = stationsToTest[i];
+    stationAbbr = stationDictionary[stationsToTest[i]];
+    //getDepartures(stationAbbr, logAdjacentTrains(stationName));
+    getDepartures(stationAbbr, drawAdjacentTrains(stationName));
+  }
+
 }
 
 $(document).ready(function() {
@@ -309,4 +498,7 @@ $(document).ready(function() {
       getDepartures(stationDictionary[$(this).attr("id")], showStationInfo);
     });
   });
+  interval = 5*1000; // 5 seconds
+  setInterval(run, interval);
 });
+
